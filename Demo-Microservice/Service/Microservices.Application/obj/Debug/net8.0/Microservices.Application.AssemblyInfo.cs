acfb8713317fe1a3f0c3441f1693ae5e53756605
Raja-1509 <rajendran159@gmail.com>
2024-12-16 18:25:17 +0000

    Initial commit to Git

--- Demo-Microservice/Service/Microservices.Application/obj/Debug/net8.0/Microservices.Application.AssemblyInfo.cs
+++ Demo-Microservice/Service/Microservices.Application/obj/Debug/net8.0/Microservices.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microservices.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979e05f102ce922b20c7de9ca9ca1cd5f0ce35d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microservices.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Microservices.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
